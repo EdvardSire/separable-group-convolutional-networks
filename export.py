fbfc8f176f609b695f476cebacfe241a4537696b
Edvard Sire <mail@edvardsire.com>
2024-05-16 18:11:57 +0200

    export vgg

--- export.py
+++ export.py
@@ -1,7 +1,10 @@
 import torch
+from vggmodel import ShapeClassifier
 
-# model = torch.load("./cp_8k_steps_ckgresnet+nonseparable+h_s-32_32_64+k-7+om-10.0+f_om-10.0+do-0.0+wd-0. 0001+n_el-8+grp-SE2+smplng-uniform+implm-SIREN+ds-SUAS.pt")
-model = torch.load("cp_8k_steps_ckgresnet+nonseparable+h_s-32_32_64+k-7+om-10.0+f_om-10.0+do-0.0+wd-0.0001+n_el-8+grp-SE2+smplng-uniform+implm-SIREN+ds-SUAS.pt",
+# model = torch.load("cp_8k_steps_ckgresnet+nonseparable+h_s-32_32_64+k-7+om-10.0+f_om-10.0+do-0.0+wd-0.0001+n_el-8+grp-SE2+smplng-uniform+implm-SIREN+ds-SUAS.pt",
+#                    map_location=torch.device("cpu")
+#                    )
+model = torch.load("VGGshape.pt",
                    map_location=torch.device("cpu")
                    )
 
@@ -10,7 +13,7 @@ sample_input = torch.randn(1,1,200,200)
 
 torch.onnx.export(model,
                   sample_input,
-                  "gcnn-gray.onnx",
+                  "VGGshape.onnx",
                   input_names=["input"],
                   output_names=["output"]
                   )
