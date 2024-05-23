cdeb86e0bf759009c1ad4c6d6e8ed91a36806eb9
EdvardSire <mail@edvardsire.com>
2024-05-23 11:39:38 +0200

    train: deeper ck

--- train.sh
+++ train.sh
@@ -4,17 +4,17 @@ python3 run_experiment.py \
     --group SE2 \
     --num_group_elements 8 \
     --sampling_method discretise \
-    --hidden_sizes 32,64,128 \
+    --hidden_sizes 32,32,64\
     --dataset SUAS \
     --epochs 1000 \
-    --batch_size 16 \
+    --batch_size 32 \
     --learning_rate 1e-4 \
     --optim adam \
     --kernel_size 7 \
     --stride 1 \
-    --dropout 0 \
+    --dropout 1 \
     --weight_decay 1e-4 \
-    --ck_net_num_hidden 2 \
+    --ck_net_num_hidden 4 \
     --ck_net_hidden_size 64 \
     --first_omega_0 10 \
     --omega_0 10 \
