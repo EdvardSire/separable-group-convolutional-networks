6983b43e2107107ffe7a5dd27ba6ed3a2ec03276
EdvardSire <mail@edvardsire.com>
2024-05-24 00:51:32 +0200

    fix: save pt with global step in the name

--- train_model.py
+++ train_model.py
@@ -103,7 +103,7 @@ def train(model, optim, scheduler, criterion, train_set, test_set, device, epoch
             # save the model on interval
             if not iteration % save_interval:
                 if model_save_path:
-                    torch.save(model, Path(writer.get_logdir()) / Path("model_iter_{}".format(iteration)).with_suffix(".pt"))
+                    torch.save(model, Path(writer.get_logdir()) / Path(f"model_iter_{step}").with_suffix(".pt"))
 
         if testing:
             val_acc = test(model, test_set, device, step, loss=criterion, writer=writer)
